36c0d64c91a43f021ce82321c1fba9b7891cd275
Mat Harvard <mat.harvard@gmail.com>
2009-10-25 23:22:26 -0700

    Added edit and delete links to posts.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,23 @@ module PostsHelper
                      :anchor => "#{post.class.to_s.downcase}_#{post.id}" })
     end
   end
+  
+  def manage_links_for(post)
+    edit_post_path = case post
+      when Article then edit_article_path(post)
+      when Audio   then edit_audio_path(post)
+      when Chat    then edit_chat_path(post)
+      when Link    then edit_link_path(post)
+      when Photo   then edit_photo_path(post)
+      when Quote   then edit_quote_path(post)
+      when Snippet then edit_snippet_path(post)
+      when Video   then edit_video_path(post)
+    end
+    
+    if signed_in?
+      content_tag(:div, :class => "manage") do
+        link_to("edit", edit_post_path) + " | " + link_to("delete", post_path(post), :method => :delete, :confirm => "Are you sure?")
+      end
+    end
+  end
 end
