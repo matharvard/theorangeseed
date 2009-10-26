3a01c8214a8d0fd8f0fd1ed2faafc01e554ed521
Mat Harvard <mat.harvard@gmail.com>
2009-10-25 23:41:30 -0700

    Added to done file, and version number.

---
+++ lib/theorangeseed/version.rb
@@ -0,0 +1,10 @@
+module Theorangeseed
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    TINY  = 0
+    
+    STRING = [MAJOR, MINOR, TINY].join(".").freeze
+    TITLE = "Untitled".freeze
+  end
+end
