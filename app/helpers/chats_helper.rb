ac38f04378f632594d7dfc94aa5487bd4e759751
Mat Harvard <mat.harvard@gmail.com>
2009-12-07 10:10:39 -0800

    New chat regexp.

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,5 +1,5 @@
 module ChatsHelper
   def format_chat(chat)
-    simple_format(chat.gsub!(/\w+:/) { |match| match = content_tag(:b, match) })
+    simple_format(chat.gsub!(/^\w+:/i) { |match| match = content_tag(:b, match) })
   end
 end
